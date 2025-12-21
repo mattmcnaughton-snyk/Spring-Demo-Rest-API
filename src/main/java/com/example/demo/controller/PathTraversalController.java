71148509561f585df20c9d93e776f56aec971511
mattmcn9 <54960795+mattmcn9@users.noreply.github.com>
2025-12-21 14:50:20 -0500

    Changed return type of file to be a Response Entity to prevent XSS

--- src/main/java/com/example/demo/controller/PathTraversalController.java
+++ src/main/java/com/example/demo/controller/PathTraversalController.java
@@ -1,13 +1,14 @@
 package com.example.demo.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -16,10 +17,18 @@ import java.nio.file.Path;
 @RestController
 public class PathTraversalController {
 
-    @GetMapping("/file")
-    public String readFile(@RequestParam String filename) throws IOException {
-        log.info("Getting File: {}", new File("/tmp/" + filename).getAbsolutePath());
-          
-        return Files.readString(Path.of("/tmp/" + filename));
+    @GetMapping(value = "/file", produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity<String> readFile(@RequestParam String filename) throws IOException {
+
+        Path path = Path.of("/tmp", filename).normalize();
+
+        log.info("File Path: {}", path);
+
+        String content = Files.readString(path);
+
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.TEXT_PLAIN)
+                .body(content);
     }
 }
